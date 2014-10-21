7f9aa5658daf2479493f1a0a16b5bf5e087fd720
Emre Yilmaz <mail@emreyilmaz.me>
2014-10-21 14:31:46 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='btcturk_client',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/emre/btcturk-client',
+    license='MIT',
+    author='Emre Yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='Btcturk.com api client',
+    install_requires=['requests'],
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
