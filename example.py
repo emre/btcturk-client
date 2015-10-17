c739958a9675c80d5b339778264fa54fa9049562
cemkiy <se.cemkiy@gmail.com>
2015-10-17 12:52:46 +0300

    transactions: changed method with get bug-fix #2

--- example.py
+++ example.py
@@ -2,4 +2,5 @@ from btcturk_client.client import Btcturk
 
 _btcturk = Btcturk("publicKey", "privateKey")
 
-print _btcturk.balance()
+print 'balance', _btcturk.balance()
+print 'transactions', _btcturk.transactions()
