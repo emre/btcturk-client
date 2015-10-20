f9f02a63c35f9d14622d36cdf82df9f9171c627b
Emre Yilmaz <emre.yilmaz@markafoni.com>
2015-10-20 10:52:35 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='btcturk_client',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     url='https://github.com/emre/btcturk-client',
     license='MIT',
